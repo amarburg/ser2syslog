59e132ffaa69750bc1fddbdd201902c6fc13e931
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2011-07-21 14:17:54 +1200

    Removed some unused declarations from devcfg.

--- devcfg.h
+++ devcfg.h
@@ -33,19 +33,6 @@ void devinit(struct termios *termctl);
 /* Converts string to baud rate */
 int string_to_baud( const char *b );
 
-/* Called to change the configuration of a device based upon the
-   string parameters. */
-/*int devconfig(char *instr, dev_info_t *info); */
-
-/* Prints the configuration of a device to a controller. */
-/*void show_devcfg(struct controller_info *cntlr, struct termios *termctl); */
-
-/* Sets the DTR and RTS lines dynamically. */
-/*int setdevcontrol(char *instr, int fd); */
-
-/* Show the state of the DTR and RTS lines. */
-/*void show_devcontrol(struct controller_info *cntlr, int fd); */
-
 /* Convert the serial parameters to a string. */
 void serparm_to_str(char *str, int strlen, struct termios *termctl);
 
